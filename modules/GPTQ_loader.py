ca67c0acbc0642e13d21151e7f5903c6bfaabfd9
mint <imbroglio@inbox.lv>
2023-04-08 14:07:37 -0500

    Fix the loader to be able to use folders

--- modules/GPTQ_loader.py
+++ modules/GPTQ_loader.py
@@ -46,11 +46,21 @@ def load_quantized(model_name):
     else:
         print("Unknown pre-quantized model type specified. Only 'llama', 'opt', 'gptj', 'gptneox' are supported")
         exit()
+    
     path_to_model = Path(f'models/{model_name}')
-    pt_model = f'{model_name}-{shared.args.gptq_bits}bit'
-
-        # Try to find the .safetensors or .pt both in the model dir and in the subfolder
-    for path in [Path(p + ext) for ext in ['.safetensors', '.pt'] for p in [f"{shared.args.model_dir}/{pt_model}", f"{path_to_model}/{pt_model}"]]:
+    found_pts = list(path_to_model.glob("*.pt"))
+    found_safetensors = list(path_to_model.glob("*.safetensors"))
+    pt_path = None
+    
+    if len(found_pts) == 1:
+        pt_path = found_pts[0]
+    elif len(found_safetensors) == 1:
+        pt_path = found_safetensors[0]
+    else: 
+        pt_model = f'{model_name}-{shared.args.gptq_bits}bit'
+    
+      # Try to find the .safetensors or .pt both in the model dir and in the subfolder
+        for path in [Path(p + ext) for ext in ['.safetensors', '.pt'] for p in [f"{shared.args.model_dir}/{pt_model}", f"{path_to_model}/{pt_model}"]]:
             if path.exists():
                 print(f"Found {path}")
                 pt_path = path
