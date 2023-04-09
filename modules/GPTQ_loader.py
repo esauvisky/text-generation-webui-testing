e171b6db2c44ca17999fee76d546ecb29593981d
mint <imbroglio@inbox.lv>
2023-04-08 19:18:01 -0500

    Finally getting somewhere after some import issues. Both autograd and v2 gptq coexist.

--- modules/GPTQ_loader.py
+++ modules/GPTQ_loader.py
@@ -5,16 +5,69 @@ from pathlib import Path
 
 import accelerate
 import torch
-
+import transformers
 import modules.shared as shared
+
+from transformers import AutoConfig, AutoModelForCausalLM
+#import llama_inference_offload
 
 sys.path.insert(0, str(Path("repositories/GPTQ-Merged/src/alpaca_lora_4bit")))
 sys.path.insert(0, str(Path("repositories/GPTQ-Merged/src/gptq_llama")))
-import llama
-#import llama_inference_offload
-import opt
-import gptneox
-import gptj
+
+#from gptq_llama import quant, modelutils
+from modelutils import find_layers
+from quant import make_quant
+
+def _load_quant(model, checkpoint, wbits, groupsize=-1, faster_kernel=False, exclude_layers=['lm_head', 'embed_out'], kernel_switch_threshold=128):
+
+    def noop(*args, **kwargs):
+        pass
+
+    config = AutoConfig.from_pretrained(model)
+    torch.nn.init.kaiming_uniform_ = noop
+    torch.nn.init.uniform_ = noop
+    torch.nn.init.normal_ = noop
+
+    torch.set_default_dtype(torch.half)
+    transformers.modeling_utils._init_weights = False
+    torch.set_default_dtype(torch.half)
+    model = AutoModelForCausalLM.from_config(config)
+    torch.set_default_dtype(torch.float)
+    model = model.eval()
+    layers = find_layers(model)
+    for name in exclude_layers:
+        if name in layers:
+            del layers[name]
+
+    gptq_args = inspect.getfullargspec(make_quant).args
+
+    make_quant_kwargs = {
+        'module': model,
+        'names': layers,
+        'bits': wbits,
+    }
+    if 'groupsize' in gptq_args:
+        make_quant_kwargs['groupsize'] = groupsize
+    if 'faster' in gptq_args:
+        make_quant_kwargs['faster'] = faster_kernel
+    if 'kernel_switch_threshold' in gptq_args:
+        make_quant_kwargs['kernel_switch_threshold'] = kernel_switch_threshold
+
+    make_quant(**make_quant_kwargs)
+    #make_quant(model, layers, wbits, groupsize)
+
+    del layers
+
+    print('Loading model ...')
+    if checkpoint.endswith('.safetensors'):
+        from safetensors.torch import load_file as safe_load
+        model.load_state_dict(safe_load(checkpoint), strict=False)
+    else:
+        model.load_state_dict(torch.load(checkpoint), strict=False)
+    model.seqlen = 2048
+    print('Done.')
+
+    return model
 
 
 
@@ -32,20 +85,6 @@ def load_quantized(model_name):
     else:
         model_type = shared.args.gptq_model_type.lower()
 
-    if model_type == 'llama':
-        if not shared.args.gptq_pre_layer:
-            load_quant = llama.load_quant
-        else:
-           load_quant = llama_inference_offload.load_quant
-    elif model_type == 'opt':
-        load_quant = opt.load_quant
-    elif model_type == 'gptneox':
-        load_quant = gptneox.load_quant
-    elif model_type == 'gptj':
-        load_quant = gptj.load_quant
-    else:
-        print("Unknown pre-quantized model type specified. Only 'llama', 'opt', 'gptj', 'gptneox' are supported")
-        exit()
     path_to_model = Path(f'models/{model_name}')
     found_pts = list(path_to_model.glob("*.pt"))
     found_safetensors = list(path_to_model.glob("*.safetensors"))
@@ -90,13 +129,14 @@ def load_quantized(model_name):
               m.bias = m.bias.half()
          autograd_4bit.use_new = True
          autograd_4bit.auto_switch = True
-       
-    # qwopqwop200's offload
-    elif shared.args.gptq_pre_layer:
-        model = load_quant(str(path_to_model), str(pt_path), shared.args.gptq_bits, shared.args.gptq_pre_layer)
+
+    # qwopqwop200's offload 
+    elif model_type == 'llama' and shared.args.pre_layer:
+        model = _load_quant(str(path_to_model), str(pt_path), shared.args.wbits, shared.args.groupsize, shared.args.pre_layer)
     else:
-        model = load_quant(str(path_to_model), str(pt_path), shared.args.gptq_bits)
-        print ('Load Quant')
+        threshold = False if model_type == 'gptj' else 128
+        model = _load_quant(str(path_to_model), str(pt_path), shared.args.wbits, shared.args.groupsize, kernel_switch_threshold=threshold)
+
         # accelerate offload (doesn't work properly)
         if shared.args.gpu_memory:
             memory_map = list(map(lambda x: x.strip(), shared.args.gpu_memory))
@@ -115,4 +155,5 @@ def load_quantized(model_name):
         elif not shared.args.cpu:
             model = model.to(torch.device('cuda:0'))
 
+    
     return model
