7d5fdd5278e3ca9a793b26d75027a2f65e8c4ed6
mint <imbroglio@inbox.lv>
2023-04-12 13:04:15 -0500

    Write out other non splitting layers for accelerate.

--- modules/GPTQ_loader.py
+++ modules/GPTQ_loader.py
@@ -188,7 +188,7 @@ def load_quantized(model_name):
                 max_memory[i] = f'{memory_map[i]}GiB' if not re.match('.*ib$', memory_map[i].lower()) else memory_map[i]
             max_memory['cpu'] = max_cpu_memory
 
-            device_map = accelerate.infer_auto_device_map(model, max_memory=max_memory, no_split_module_classes=["LlamaDecoderLayer"])
+            device_map = accelerate.infer_auto_device_map(model, max_memory=max_memory, no_split_module_classes=["LlamaDecoderLayer", "GPTJBlock", "OPTDecoderLayer", "GPTNeoXLayer"])
             print("Using the following device map for the 4-bit model:", device_map)
             # https://huggingface.co/docs/accelerate/package_reference/big_modeling#accelerate.dispatch_model
             model = accelerate.dispatch_model(model, device_map=device_map, offload_buffers=True)
